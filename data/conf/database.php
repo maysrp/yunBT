d807ef5bb09e427ae918675fd188edcd961f6ba5
xiaohai <maysrp@gmail.com>
2018-12-20 13:01:16 +0800

    DATA director

---
+++ data/conf/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => 'yunbt',
+    // 用户名
+    'username' => 'yunbt',
+    // 密码
+    'password' => 'a123456',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'yunbt_',
+    "authcode" => 'oGjRF9yk8WDljihUUI',
+    //#COOKIE_PREFIX#
+];
